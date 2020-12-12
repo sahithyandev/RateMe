e4a845bb53cf171662d232d92358017c350078b2
Sahithyan <sahithyan2701@gmail.com>
2020-12-12 20:01:06 +0530

    project initiated

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "./style/main.scss";
+
+export const App = () => {
+  return <div></div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("app-root"));
