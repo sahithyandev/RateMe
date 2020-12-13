afbe34a5e1ec95acc3e031dc258af35cbd50603c
Sahithyan <sahithyan2701@gmail.com>
2020-12-13 05:38:20 +0530

    added react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import CreateBoardPage from "./pages/CreateBoard";
 import "./style/main.scss";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/create">
+          <CreateBoardPage />
+        </Route>
+        <Route path="/board/:id"></Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("app-root"));
---
+++ src/pages/CreateBoard.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+// import formik after adding
+
+const CreateBoardPage = (props) => {
+  return <div></div>;
+};
+
+export default CreateBoardPage;
