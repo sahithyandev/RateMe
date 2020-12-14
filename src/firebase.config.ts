b11b7c1ab35ded10730a327f8735fc62199b40f4
Sahithyan <sahithyan2701@gmail.com>
2020-12-14 22:35:11 +0530

    firebase added

---
+++ src/firebase.config.ts
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAVBIOWJo_Unx5iKXw7I9SMNQz8vE0HF6o",
+  authDomain: "rate-me-efc08.firebaseapp.com",
+  projectId: "rate-me-efc08",
+  storageBucket: "rate-me-efc08.appspot.com",
+  messagingSenderId: "332219223085",
+  appId: "1:332219223085:web:61ad366ba2523fe316fca4",
+};
+
+firebase.initializeApp(firebaseConfig);
