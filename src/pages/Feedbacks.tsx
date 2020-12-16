8f9724799da86397cf13b4ae06ec56d17785ce41
Sahithyan <sahithyan2701@gmail.com>
2020-12-16 09:41:17 +0530

    working on feedbacks page

--- src/pages/Feedbacks.tsx
+++ src/pages/Feedbacks.tsx
@@ -1,23 +1,40 @@
 import * as React from "react";
 import { useParams } from "react-router-dom";
+import { Typography } from "antd";
+
 import { log } from "./../global";
-import { Title } from "../components";
 import { FeedbackObj, BoardObj } from "./../types";
+
+const { Title, Text, Paragraph } = Typography;
 
 export const FeedbacksPage = (props) => {
   const { id: boardId } = useParams<{ id: string }>();
-  const [boardData, setBoardData] = React.useState<BoardObj>();
-  const [feedbacks, setFeedbacks] = React.useState<FeedbackObj[]>();
+  const [boardData, setBoardData] = React.useState<BoardObj>({
+    name: "Michelle Lynn",
+    description:
+      "Urna ut volutpat egestas amet posuere pellentesque molestie sagittis nisi",
+    feedbackCount: 2,
+  });
+  const [feedbacks, setFeedbacks] = React.useState<FeedbackObj[]>([]);
   log(boardId);
 
-  const fetchData = () => {
+  const fetchData = (): FeedbackObj[] => {
     // TODO: implement this function
-    return [];
+    return [
+      {
+        message:
+          "Nice guy. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur ipsa quis minus nostrum laboriosam vero harum itaque vitae laudantium doloremque.",
+      },
+      {
+        message:
+          "Tenetur ipsa quis minus nostrum laboriosam vero harum itaque vitae laudantium doloremque.",
+      },
+    ];
   };
 
   React.useEffect(() => {
     setFeedbacks(fetchData());
-  });
+  }, []);
 
   const generateFeedbackCard = (feedback: FeedbackObj) => {
     return (
@@ -29,8 +46,14 @@ export const FeedbacksPage = (props) => {
 
   return (
     <div className="page" id="feedbacks-page">
-      <Title>{boardData.name}</Title>
-      {boardData.feedbackCount === 0 ? null : (
+      <div className="header">
+        <Title>{boardData.name}</Title>
+        <Paragraph>{boardData.description}</Paragraph>
+      </div>
+
+      {boardData.feedbackCount === 0 ? (
+        <Text className="no-feedback-message">No feedback so far.</Text>
+      ) : (
         <div className="feedbacks-container">
           {feedbacks.map(generateFeedbackCard)}
         </div>
